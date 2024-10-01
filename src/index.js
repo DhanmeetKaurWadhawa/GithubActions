99cfdead116e19281ae3ed00b034be03aa0d6db6
Dhanmeet <dhanmeetwadhawa@gmail.com>
2024-10-01 10:58:18 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-const core = require('@actions/core');
- 
-const person=process.env.INPUT_PERSON;
-console.log('Hello ${person} from custom js actions');
+console.log('hello'); 
+const core = require('@actions/core'); 
+try { const person = process.env.INPUT_PERSON; 
+console.log(`Hello ${person} from custom js actions`); // Use backticks for string interpolation 
+} 
+catch (error) { 
+    core.setFailed(`Action failed: ${error}`);
+ }
