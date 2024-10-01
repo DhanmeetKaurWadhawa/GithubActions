f59b52c065e0ba246909520ab31f6707acb4c618
Dhanmeet <dhanmeetwadhawa@gmail.com>
2024-10-01 12:00:53 +0530

    docker file added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 console.log('hello'); 
 const core = require('@actions/core'); 
 try { const person = process.env.INPUT_PERSON; 
-console.log(`Hello ${person} from custom js actions`); // Use backticks for string interpolation 
+console.log(`Hello ${person} from custom js actions`); 
 } 
 catch (error) { 
     core.setFailed(`Action failed: ${error}`);
